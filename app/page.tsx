04dc5fee42b38399e4123e8a4bf9e94d87787140
Vincent Hesivert <vincent.hesivert@outlook.fr>
2026-02-27 14:40:48 +0100

    New HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { NextPage } from "next";
 import Link from "next/link";
+import "./global.scss";
 
 const Home: NextPage = () => {
   return (
     <div id="home_page">
       <h1>Bienvenue sur le pokédex !</h1>
+      <img src="/Who'sThatPokemon.png" alt="Who's that pokemon" />
       <Link href={"/pokemons"}>
         <button>Accéder à la liste de pokémons</button>
       </Link>
